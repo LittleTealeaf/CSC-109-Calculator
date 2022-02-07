3ee27fef4fb07154b269352a29580c7d285459ef
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-07 16:16:18 -0500

    Added option to add or subtract

--- calculator.py
+++ calculator.py
@@ -1,7 +1,17 @@
 
 def prompt_values():
-	return (int(input("Enter the first digit: ")), int(input("Enter the second digit: ")))
+	return (int(input("Enter the first integer: ")), int(input("Enter the second integer: ")))
 
 a,b = prompt_values()
-print(a + b)
+
+print("""
+Would you like to add or subtract your numbers?
+a. Add
+s. Subtract
+""")
+val = input()
+if val == "a":
+	print(a + b)
+elif val == "b":
+	print(a - b)
 
